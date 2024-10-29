1a493d4359460fd143eb71f286e1a19e6a2e4d93
vitorGabr <macstudios.info@gmail.com>
2024-10-29 16:37:56 -0300

    Refactor route.ts to export GET function

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,6 +1,6 @@
 import { revalidatePath } from "next/cache";
 
-export default function GET(){
+export function GET(){
     revalidatePath('/')
     return Response.json({message: 'Hello World!'});
 }
