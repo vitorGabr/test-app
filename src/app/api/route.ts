fdd09c00ad749d31f35bb392c70c415ef13abebf
vitorGabr <macstudios.info@gmail.com>
2024-10-30 13:07:43 -0300

    Refactor API route to use revalidatePath and remove cacheTag from Home component

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,6 +1,6 @@
 import { revalidatePath, revalidateTag } from "next/cache";
 
 export function GET(){
-    revalidateTag('hello-world');
+    revalidatePath('/','page');
     return Response.json({message: 'Hello World!'});
 }
