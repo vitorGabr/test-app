440b4647130bcc5b1be6bbae8b4382b8b7ca407a
vitorGabr <macstudios.info@gmail.com>
2024-10-31 10:24:26 -0300

    Set max cache duration in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use cache"
 import { unstable_cacheLife as cacheLife } from 'next/cache'
-cacheLife('max')
 
 export default async function Home() {
+  cacheLife('max')
   const randomNum = Math.floor(Math.random() * 1000);
 
 	return (
