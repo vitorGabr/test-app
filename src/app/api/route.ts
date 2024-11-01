2d9e1e898c2f3801e36781e6949b1b6d0d0adbee
vitorGabr <macstudios.info@gmail.com>
2024-11-01 08:06:56 -0300

    Remove unused API route and refactor layout and page components for improved caching and styling

--- src/app/api/route.ts
+++
@@ -1,6 +0,0 @@
-import { revalidatePath, revalidateTag } from "next/cache";
-
-export function GET(){
-    revalidatePath('/','page');
-    return Response.json({message: 'Hello World!'});
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-"use cache"
-import { unstable_cacheLife as cacheLife } from 'next/cache'
-
-export default async function Home() {
-  cacheLife('max')
-  const randomNum = Math.floor(Math.random() * 1000);
-
+export default function Home() {
 	return (
 		<main className="w-full h-screen flex flex-col items-center justify-center">
-      <h1>{randomNum}</h1>
-    </main>
+			<h1>{'asd'}</h1>
+		</main>
 	);
 }
